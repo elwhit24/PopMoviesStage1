6c9756fb536069dd7ddb3e46c93e213707f5f71e
[Mike W.} <[elwhit24@gmail.com]>
2016-05-09 16:55:31 -0700

    remove commented fab code

--- app/src/main/java/com/example/michelangelowhitten/popmoviesstage1/MainActivity.java
+++ app/src/main/java/com/example/michelangelowhitten/popmoviesstage1/MainActivity.java
@@ -25,15 +25,6 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar1);
         setSupportActionBar(toolbar);
 
-        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        assert fab != null;
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "this app is in development!", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
     }
 
     @Override
