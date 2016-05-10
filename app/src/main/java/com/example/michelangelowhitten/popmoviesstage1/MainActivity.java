80f20a7602511b688ac4af79a6ba8393750ce197
[Mike W.} <[elwhit24@gmail.com]>
2016-05-09 17:31:11 -0700

    replace strings.xml file in res/values with one without any qualifiers which is what is referenced to in manifest with app_name

--- app/src/main/java/com/example/michelangelowhitten/popmoviesstage1/MainActivity.java
+++ app/src/main/java/com/example/michelangelowhitten/popmoviesstage1/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.fragment_main);
+        setContentView(R.layout.activity_main);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar1);
         setSupportActionBar(toolbar);
