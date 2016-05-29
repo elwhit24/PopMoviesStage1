1e3753bca12318b327807ae893eb2e87075a42a3
[Mike W.} <[elwhit24@gmail.com]>
2016-05-29 10:10:23 -0700

    work on recycleView and details fragment

--- app/src/main/java/com/example/michelangelowhitten/popmoviesstage1/MainActivity.java
+++ app/src/main/java/com/example/michelangelowhitten/popmoviesstage1/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
@@ -29,13 +31,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FragmentManager fragManager = getFragmentManager();
-        FragmentTransaction fragTransaction = fragManager.beginTransaction()
-                .replace(R.id.container, new MainActivityFragment());
-        fragTransaction.commit();
-        fragTransaction.replace(R.id.container, new MainActivityFragment());
-
-        Log.d(TAG, "after transaction commit");
+        if (savedInstanceState == null) {
+            FragmentManager fragManager = getFragmentManager();
+            FragmentTransaction fragTransaction = fragManager.beginTransaction()
+                    .replace(R.id.container, new MainActivityFragment());
+            fragTransaction.commit();
+            fragTransaction.replace(R.id.container, new MainActivityFragment());
+        } else {
+            Bundle bundle = savedInstanceState.getBundle(TAG);
+            super.onCreate(bundle);
+            setContentView(R.layout.activity_main);
+        }
+            /*Bundle nBundle = new Bundle();
+            bundle.putAll(nBundle);*/
+            Log.d(TAG, "after transaction commit");
     }
 
     @Override
@@ -46,23 +55,24 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(AndroidMovie movieItem) {
+    //@Override
+    public boolean onOptionsItemSelected(Menu menuItem) {
 
-        Bundle args = new Bundle();
-        args.putParcelable(DetailsFragment.DETAIL_MOVIE, movieItem);
+        if (menuItem == null) {
+            Bundle args = new Bundle();
+            args.putParcelable(DetailsFragment.DETAIL_MOVIE, (Parcelable) menuItem);
 
-        DetailsFragment dFrag = new DetailsFragment();
-        dFrag.setArguments(args);
+            Fragment dFrag = new Fragment();
+            dFrag.setArguments(args);
 
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.movie_detail, dFrag, DetailsFragment.D_TAG)
-                .commit();
-        int id = movieItem.getMovieId();
-        if (id == R.id.action_settings) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.movie_detail, dFrag, DetailsFragment.D_TAG)
+                    .commit();
+        } else {
             startActivity(new Intent(this, Settings.class));
             return true;
         }
+
         return true;  //super.onOptionsItemSelected(movieItem);
     }
 }
