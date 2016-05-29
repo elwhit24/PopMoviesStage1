ff089130cfe501a95a86af67b0759d49c91b9659
[Mike W.} <[elwhit24@gmail.com]>
2016-05-29 10:46:01 -0700

    work on recycleView and details fragment

--- app/src/main/java/com/example/michelangelowhitten/popmoviesstage1/PosterAdapter.java
+++ app/src/main/java/com/example/michelangelowhitten/popmoviesstage1/PosterAdapter.java
@@ -2,9 +2,12 @@ package com.example.michelangelowhitten.popmoviesstage1;
 
 /**
  * Created by Mike on 5/5/2016.
+ * lots of info&code snippets from developer site:
+ * https://developer.android.com/training/material/lists-cards.html
  */
 import android.content.Context;
 import android.content.Intent;
+import android.media.Image;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,23 +19,25 @@ import com.squareup.picasso.Picasso;
 
 public class PosterAdapter extends RecyclerView.Adapter<PosterAdapter.ViewHolder> {
 
-    private ArrayList<AndroidMovie> mDataSet;
+    private ArrayList<AndroidMovie> movieArrayList;
+    private Context context;
 
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder
     public static class ViewHolder extends RecyclerView.ViewHolder {
         // each data item is just a string in this case
-        public TextView mTextView;
-        public ViewHolder(TextView v) {
+        public View view;
+        public ViewHolder(View v) {
             super(v);
-            mTextView = v;
+            view = v;
         }
     }
 
     // Provide a suitable constructor (depends on the kind of dataset)
-    public PosterAdapter(ArrayList<AndroidMovie> myDataset) {
-        mDataset = myDataset;
+    public PosterAdapter(Context context, ArrayList<AndroidMovie> movieArrayList) {
+        this.movieArrayList = movieArrayList;
+        this.context = context;
     }
 
     // Create new views (invoked by the layout manager)
@@ -41,9 +46,9 @@ public class PosterAdapter extends RecyclerView.Adapter<PosterAdapter.ViewHolder
                                                    int viewType) {
         // create a new view
         View v = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.my_text_view, parent, false);
+                .inflate(R.layout.movie_row, parent, false);
         // set the view's size, margins, paddings and layout parameters
-        ...
+
         ViewHolder vh = new ViewHolder(v);
         return vh;
     }
@@ -53,14 +58,32 @@ public class PosterAdapter extends RecyclerView.Adapter<PosterAdapter.ViewHolder
     public void onBindViewHolder(ViewHolder holder, int position) {
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
-        holder.mTextView.setText(mDataset[position]);
 
+
+        final ImageView imageView = (ImageView) holder.view.findViewById(R.id.movie_poster);
+
+        for(int i = 0; i < movieArrayList.size(); i++) {
+            Picasso.with(context).
+                    load(movieArrayList.get(i).getPosterImageUrl()).
+                    noFade().
+                    fit().
+                    centerCrop().
+                    into(imageView);
+        }
     }
 
     // Return the size of your dataset (invoked by the layout manager)
     @Override
     public int getItemCount() {
-        return mDataset.length;
+        return movieArrayList.size();
+    }
+
+    public ArrayList<AndroidMovie> getMovieArrayList() {
+        return movieArrayList;
+    }
+
+    public void setMovieArrayList(ArrayList<AndroidMovie> movieArrayList) {
+        this.movieArrayList = movieArrayList;
     }
 }
 
@@ -68,12 +91,7 @@ public class PosterAdapter extends RecyclerView.Adapter<PosterAdapter.ViewHolder
         .load(movieItem.getPosterImageUrl())
         .into(imageView);*/
 
-/*Picasso.with(context).
-                load(movieItem.getPosterImageUrl()).
-                noFade().
-                fit().
-                centerCrop().
-                into(imageView);*/
+
 
 /*public ArrayList<AndroidMovie> getPopMovieArrayList() {
         return popMovieArrayList;
