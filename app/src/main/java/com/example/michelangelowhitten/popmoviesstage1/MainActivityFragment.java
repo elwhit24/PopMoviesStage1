ebb14911a690bf36787377dfb1a32e84a516961b
[Mike W.} <[elwhit24@gmail.com]>
2016-06-16 13:27:18 -0700

    continue work on view

--- app/src/main/java/com/example/michelangelowhitten/popmoviesstage1/MainActivityFragment.java
+++ app/src/main/java/com/example/michelangelowhitten/popmoviesstage1/MainActivityFragment.java
@@ -1,17 +1,12 @@
 package com.example.michelangelowhitten.popmoviesstage1;
 
 import android.app.Fragment;
-import android.content.ContentResolver;
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
-import android.database.Cursor;
 import android.media.Image;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.net.Uri;
 import android.os.AsyncTask;
-import android.os.StrictMode;
 import android.preference.PreferenceManager;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
@@ -21,27 +16,18 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.GridLayout;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.net.URL;
-import java.nio.charset.Charset;
 import java.util.ArrayList;
-import java.util.zip.Inflater;
 
-/*created by Michelangelo Whitten over a period of many months of learning*/
+/*created by Michelangelo Whitten*/
 
 public class MainActivityFragment extends Fragment {
 
